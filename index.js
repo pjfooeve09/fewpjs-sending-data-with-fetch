5ea58284958f07c1505034668017a82ab7394687
Parham Javadi <pjfooeve09@yahoo.com>
2021-06-22 19:41:28 -0700

    commit tree 53423339f76c3a5e4406bab84504673846034851

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let data = {
+        name: `${userName}`,
+        email: `${userEmail}`
+      };
+    let configObj = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            let id = object.id.toString()
+            const p = document.createElement('p');
+            p.innerHTML = `${id}`
+            document.body.appendChild(p);
+        })
+        .catch(function(error) {
+            alert("Error alert!");
+            const p = document.createElement('p');
+            p.innerHTML = `${error.message}`
+            document.body.appendChild(p);
+        });
+}
